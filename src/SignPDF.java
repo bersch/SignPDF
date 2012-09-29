e39da8405c8ed5f607dc287bb93680e776bb1122
Bernhard Schneider <bernhard@neaptide.org>
2012-09-29 13:23:16 +0200

    new legal notice, some makeup

--- src/SignPDF.java
+++ src/SignPDF.java
@@ -5,21 +5,17 @@
 //
 // Author: Bernhard Schneider <bernhard@neaptide.org>
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
-// $Id: SignPDF.java,v 1.6.2.1 2012-05-17 19:36:16 bs Exp $
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 
@@ -244,7 +240,8 @@ public class SignPDF {
         stamper.setEncryption(true, null, null,
         		PdfWriter.ALLOW_PRINTING | PdfWriter.ALLOW_SCREENREADERS | PdfWriter.ALLOW_COPY);
         
-        // Producer ?
+        // FIXME: this method doesn't work like expected,
+        //        produce information should be visible in the pdf-info dialog 
         HashMap info = reader.getInfo();
         info.put("Producer", "SingPDF " + version + " " + (String)info.get("Producer"));
         stamper.setMoreInfo(info);
