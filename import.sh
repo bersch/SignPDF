d9f3bec9580daaa2a4997af77c8414a8b819f843
Bernhard Schneider <bernhard@neaptide.org>
2012-09-29 12:43:01 +0200

    small cleanup

---
+++ import.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# vim:enc=latin1:tenc=latin1
+
+if [ -z $1 ]; then
+echo "Usage: $0 <CA cert PEM file>"
+  exit 1
+fi
+
+CACERT=$1
+BCJAR=/usr/share/java/bcprov.jar
+
+
+TRUSTSTORE=.keystore
+ALIAS=`openssl x509 -inform PEM -subject_hash -noout -in "$CACERT"`
+
+rm "$TRUSTSTORE" 
+
+echo "Adding certificate to $TRUSTSTORE..."
+keytool -import -v -trustcacerts -alias $ALIAS \
+      -file $CACERT \
+      -keystore $TRUSTSTORE \
+	  -storepass swordfish
+
+echo ""
+echo "Added '$CACERT' with alias '$ALIAS' to $TRUSTSTORE..."
+
+echo "checking ..."
+
+keytool -list -keystore "$TRUSTSTORE" -storepass swordfish
+
---
+++ src/SignPDF.java
@@ -0,0 +1,332 @@
+//
+// SignPDF.java
+//
+// Usage: java SignPDF pdfdocument.pdf
+//
+// Author: Bernhard Schneider <bernhard@neaptide.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// $Id: SignPDF.java,v 1.6.2.1 2012-05-17 19:36:16 bs Exp $
+//
+
+
+//import java.security.spec.*;
+//import java.security.cert.*;
+import java.io.*;
+import java.util.*;
+import java.security.*;
+
+/* chmod */
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+import org.bouncycastle.*;
+import org.bouncycastle.tsp.*;
+import org.bouncycastle.tsp.TimeStampRequest;
+import org.bouncycastle.tsp.TimeStampToken;
+import org.bouncycastle.tsp.TimeStampResponseGenerator;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.security.cert.CertificateFactory;
+
+// import java.io.FileInputStream;
+// import java.io.FileOutputStream;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+
+import com.itextpdf.text.pdf.PdfString;
+import com.itextpdf.text.pdf.PdfSignature;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfDate;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfPKCS7;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfWriter;
+
+
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfStamper;
+
+import com.itextpdf.text.pdf.*;
+
+
+public class SignPDF {
+
+	static String version = "0.3";	
+	
+	static String rcname = System.getenv("HOME") + "/.signpdf";
+	static Properties config = new Properties();
+	private static final long TICKS_PER_DAY = 1000 * 60 * 60 * 24;
+	
+	static String path;
+	static String  src;
+	static String dest;
+	static String keystore_password;
+	static String key_password;
+	static String reason;
+	static String location;
+	static String contact;
+	static String alias;
+	static String tsa_url;
+	static String tsa_login;
+	static String tsa_passw;
+	static String http_proxy_host;
+	static String http_proxy_port;
+	static String https_proxy_host;
+	static String https_proxy_port;
+	static String root_cert;
+	
+	
+	
+	static char[] readPassword(String msg) throws RuntimeException {
+		
+		Console cons = System.console();
+		if (cons == null) 
+			throw new RuntimeException("can't continue w/o console");
+		char[] pwd = cons.readPassword("%s: ", msg );
+		return pwd;
+	}
+	
+	public static boolean getProperties()
+		throws FileNotFoundException, IOException {
+		try {
+			config.load(new FileInputStream(rcname));
+		} catch (IOException e) {
+			return false;
+		}	
+
+		path              = config.getProperty("keystore");
+		keystore_password = config.getProperty("keystore_password");
+		key_password      = config.getProperty("keystore_key_password");
+		alias             = config.getProperty("keystore_key_alias");
+		reason            = config.getProperty("reason");
+		location          = config.getProperty("location");
+		contact           = config.getProperty("contact");
+		tsa_url           = config.getProperty("tsa_url");
+		tsa_login         = config.getProperty("tsa_login");
+		tsa_passw         = config.getProperty("tsa_passw");
+		root_cert         = config.getProperty("root_cert");
+		http_proxy_host   = config.getProperty("http_proxy_host");
+		http_proxy_port   = config.getProperty("http_proxy_port");
+		https_proxy_host  = config.getProperty("https_proxy_host");
+		https_proxy_port  = config.getProperty("https_proxy_port");
+		
+		if (path.length() == 0) 
+			throw new FileNotFoundException("can't continue w/o keystore.");
+		
+		// read phrase if "", otherwise is null
+		if (keystore_password.length() == 0) 
+			keystore_password = new String(readPassword("keystore_password"));
+		
+		// read phrase if "", otherwise is null
+		if (key_password.length() == 0) 
+			key_password = new String(readPassword("keystore_key_password"));
+
+		// proxies
+		boolean use_proxy = false;
+		if (http_proxy_host != null && http_proxy_host.length() > 0
+				&& http_proxy_port != null && http_proxy_port.length() > 0) {
+				use_proxy = true;
+		    	System.setProperty("http.proxyHost",http_proxy_host);
+		    	System.setProperty("http.proxyPort",http_proxy_port);
+			}
+		if (https_proxy_host != null && https_proxy_host.length() > 0
+				&& https_proxy_port != null && https_proxy_port.length() > 0) {
+				use_proxy = true;
+		    	System.setProperty("https.proxyHost",https_proxy_host);
+		    	System.setProperty("https.proxyPort",https_proxy_port);
+		}
+
+		if (use_proxy)
+			System.setProperty("java.net.useSystemProxies", "true");
+
+		return true;
+	}
+	
+	interface CLibrary extends Library {
+		public int chmod(String path, int mode);
+	}
+	
+	public static void createDefaultProperties() {
+		CLibrary libc = (CLibrary) Native.loadLibrary("c", CLibrary.class);	
+		
+		try {
+			FileOutputStream out = new FileOutputStream(rcname);
+			
+			config.setProperty("keystore","/path/to/keystore");
+			config.setProperty("keystore_password", "");
+			config.setProperty("keystore_key_password", "");
+			config.setProperty("keystore_key_alias", "");
+			config.setProperty("reason", "some corp certified");
+			config.setProperty("location", "location");
+			config.setProperty("contact", "name <name@domain.tld>");
+			config.setProperty("tsa_url", "");
+			config.setProperty("tsa_login", "");
+			config.setProperty("tsa_passw", "");
+			config.setProperty("root_cert", "");
+			config.setProperty("http_proxy_host","");
+			config.setProperty("http_proxy_port","");
+			config.setProperty("https_proxy_host","");
+			config.setProperty("https_proxy_port","");
+			
+			config.store(out, " " + rcname);
+			libc.chmod(rcname, 0600);
+			System.err.println("Configuration file created (" + rcname + "). Please edit it first!");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public static void main(String args[]) {
+		try {
+
+		if (args.length != 1) {
+			System.err.println("usage: $0 <pdf-file>");
+			System.exit(1);
+		}
+		src = args[0];
+		dest = src + ".temp";
+		
+		if (!getProperties()) 
+			createDefaultProperties();
+				
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(new FileInputStream(path), keystore_password.toCharArray());
+        if (alias == null || alias.length() == 0) 
+        	alias = (String)ks.aliases().nextElement();
+        Certificate[] chain = ks.getCertificateChain(alias);
+        PrivateKey key = (PrivateKey) ks.getKey(alias, key_password.toCharArray());        
+        
+        X509Certificate cert = (X509Certificate)ks.getCertificate(alias);
+        System.out.println("Signer ID serial     " + cert.getSerialNumber());
+        System.out.println("Signer ID version    " + cert.getVersion());
+        System.out.println("Signer ID issuer     " + cert.getIssuerDN());
+        System.out.println("Signer ID not before " + cert.getNotBefore());        
+        System.out.println("Signer ID not after  " + cert.getNotAfter());
+        
+        // show days valid
+        long ticks_now  = new Date().getTime();
+        long ticks_to   = cert.getNotAfter().getTime();
+        
+        long ticks_delta = ( ticks_to - ticks_now ) / TICKS_PER_DAY;
+        System.out.println("Certificate will expire in " + ticks_delta + " days.");
+
+        // Verify sig
+        Signature s = Signature.getInstance("SHA1withRSA");
+        s.initVerify(ks.getCertificate(alias));
+
+        // check validity
+        cert.checkValidity();
+        
+        PdfReader reader = new PdfReader(src);
+        FileOutputStream os = new FileOutputStream(dest);
+        //PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0', null, false);
+        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+        stamper.setEncryption(true, null, null,
+        		PdfWriter.ALLOW_PRINTING | PdfWriter.ALLOW_SCREENREADERS | PdfWriter.ALLOW_COPY);
+        
+        // Producer ?
+        HashMap info = reader.getInfo();
+        info.put("Producer", "SingPDF " + version + " " + (String)info.get("Producer"));
+        stamper.setMoreInfo(info);
+        
+        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+        // stamper.close();
+        
+        appearance.setReason(reason);
+        appearance.setLocation(location);
+        appearance.setContact(contact);
+        appearance.setCrypto(key, chain, null, PdfSignatureAppearance.WINCER_SIGNED);
+        appearance.setCertificationLevel(PdfSignatureAppearance.CERTIFIED_NO_CHANGES_ALLOWED);
+
+        /// ts + ocsp
+    	PdfPKCS7 sgn = new PdfPKCS7(key, chain, null, "SHA1", null, false);
+    	PdfSignature dic = new PdfSignature(PdfName.ADOBE_PPKLITE, new PdfName("adbe.pkcs7.detached"));
+    	dic.setReason(appearance.getReason());
+    	dic.setLocation(appearance.getLocation());
+    	dic.setContact(appearance.getContact());
+    	dic.setDate(new PdfDate(appearance.getSignDate()));
+    	appearance.setCryptoDictionary(dic);
+        
+        
+        // timestamping + ocsp
+        
+        if (tsa_url != null && tsa_url.length() > 0) {
+        	
+        	byte[] ocsp  = null;
+        	TSAClient tsc = null;
+        
+        	int contentEstimated = 15000;
+        	HashMap exc = new HashMap();
+        	exc.put(PdfName.CONTENTS, new Integer(contentEstimated * 2 + 2));
+        	appearance.preClose(exc);
+                	
+        	InputStream data = appearance.getRangeStream();
+        	MessageDigest mdig = MessageDigest.getInstance("SHA1");
+        	
+        	byte buf[] = new byte[8192];
+        	int n;
+        	while ((n = data.read(buf)) > 0) {
+        		mdig.update(buf, 0, n);
+        	}
+        	byte hash[]  = mdig.digest();
+        	Calendar cal = Calendar.getInstance();
+        	
+        	
+        	if (root_cert != null && root_cert.length() > 0) {
+        		String url = PdfPKCS7.getOCSPURL((X509Certificate)chain[0]);
+        		CertificateFactory cf = CertificateFactory.getInstance("X509");
+                FileInputStream is = new FileInputStream(root_cert);
+                X509Certificate root = (X509Certificate) cf.generateCertificate(is);
+                ocsp = new OcspClientBouncyCastle().getEncoded((X509Certificate)chain[0], root, url);
+        	}
+
+        	byte sh[]    = sgn.getAuthenticatedAttributeBytes(hash, cal, ocsp);
+        	sgn.update(sh, 0, sh.length);
+        	
+        	if (tsa_url != null && tsa_url.length() > 0) {
+        		tsc = new TSAClientBouncyCastle(tsa_url, tsa_login, tsa_passw);
+        		byte[] encodedSig = sgn.getEncodedPKCS7(hash, cal, tsc, ocsp);
+        		if (contentEstimated + 2 < encodedSig.length) 
+        			throw new Exception("Not enough space");
+        		byte[] paddedSig = new byte[contentEstimated];
+        		System.arraycopy(encodedSig, 0, paddedSig, 0, encodedSig.length);
+        		PdfDictionary dic2 = new PdfDictionary();
+        		dic2.put(PdfName.CONTENTS, new PdfString(paddedSig).setHexWriting(true));
+        		appearance.close(dic2);
+        	}
+        }
+        //// 
+        
+        // stamper.close();
+        File mysrc = new File(src); mysrc.delete();
+        File mydest = new File(dest); mydest.renameTo(mysrc);
+        
+        System.exit(0);
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
